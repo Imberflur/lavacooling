ce2dc21d91c021e49501ec0c7c2237fa306b6852
Rath <rathsaer@gmail.com>
2012-08-08 01:38:08 -0400

    add pumice and a few tweaks

--- init.lua
+++ init.lua
@@ -1,9 +1,9 @@
-COOLING_RATE = 10
+COOLING_RATE = 60
 IRON_FORMATION_RATE = 32
 --Molten rock takes 10 times longer to turn into stone than lava takes to turn into molten rock.
 COOlING_RATIO = 10
 
---Nodes/Items/Fuels
+--Nodes/Items
 minetest.register_node("lavacooling:moltenrock", {
 	description = "Molten Rock",
 	inventory_image = minetest.inventorycube("lavacooling_moltenrock.png"),
@@ -14,12 +14,6 @@ minetest.register_node("lavacooling:moltenrock", {
 	light_source = 10,
 	groups = {cracky=3, hot=3, igniter=1},
 		sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_craft({
-	type = "fuel",
-	recipe = "lavacooling:moltenrock",
-	burntime = 30,
 })
 
 minetest.register_craftitem("lavacooling:obsidian_shard", {
@@ -33,18 +27,34 @@ minetest.register_node("lavacooling:obsidian", {
 	groups = {cracky=2},
 	sounds = default.node_sound_stone_defaults(),
 	drop = {
-		max_items = 1,
 		items = {
 			{
-				items = {"lavacooling:obsidian_shard"},
-				rarity = 2,
-			},
-			{
 			items = {"lavacooling:obsidian"},
-			rarity = 8,
+			rarity = 6,
 			},
-		}
+		},
 	},
+})
+
+minetest.register_node("lavacooling:pumice", {
+	description = "Pumice",
+	tiles = {"lavacooling_pumice.png"},
+	groups = {cracky=2},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+--Crafts
+minetest.register_craft({
+	type = "fuel",
+	recipe = "lavacooling:moltenrock",
+	burntime = 30,
+})
+
+minetest.register_craft({
+	output = "lavacooling:obsidian_shard 3",
+	recipe = {
+		{"lavacooling:obsidian"},
+	}
 })
 
 --ABMs
@@ -64,7 +74,12 @@ minetest.register_abm ({
 	interval = 1.0,
 	chance = 1,
 	action = function (pos)
+		if minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "default:water_source" or minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "default:water_flowing"
+		then
+		minetest.env: add_node (pos, {name = "lavacooling:pumice"})
+		else
 		minetest.env: add_node (pos, {name = "default:stone"})
+		end
 	end,
 })
 
@@ -72,7 +87,7 @@ minetest.register_abm ({
 	nodenames = {"default:lava_source", "default:lava_flowing"},
 	neighbors = {"air"},
 	interval = 5.0,
-	chance = COOLING_RATE*6,
+	chance = COOLING_RATE,
 	action = function (pos)
 		minetest.env: add_node (pos, {name = "lavacooling:moltenrock"})
 	end,
@@ -80,17 +95,22 @@ minetest.register_abm ({
 
 minetest.register_abm ({
 	nodenames = {"lavacooling:moltenrock"},
-	interval = 10.0,
-	chance = COOLING_RATE*COOlING_RATIO*3,
+	interval = 5.0,
+	chance = COOLING_RATE*COOlING_RATIO,
 	action = function (pos)
+		if minetest.env: get_node({x=pos.x, y=pos.y+1, z=pos.z}).name == "air" and minetest.env: get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "lavacooling:moltenrock"
+		then
+		minetest.env: add_node (pos, {name = "lavacooling:pumice"})
+		else
 		minetest.env: add_node (pos, {name = "default:stone"})
+		end
 	end,
 })
 
 minetest.register_abm ({
 	nodenames = {"lavacooling:moltenrock"},
-	interval = 10.0,
-	chance = COOLING_RATE*IRON_FORMATION_RATE*COOlING_RATIO*3,
+	interval = 5.0,
+	chance = COOLING_RATE*IRON_FORMATION_RATE*COOlING_RATIO,
 	action = function (pos)
 		minetest.env: add_node (pos, {name = "default:stone_with_iron"})
 	end,
